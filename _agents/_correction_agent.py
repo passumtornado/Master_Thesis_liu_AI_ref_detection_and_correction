ec390ca0a3661de39969a5997e1347f96ce28277
passumtornado <passumtornado10@gmail.com>
2026-05-07 12:50:17 +0200

    new update

--- _agents/_correction_agent.py
+++ _agents/_correction_agent.py
@@ -243,9 +243,9 @@ class CorrectionAgent:
         
         self.llm = ChatOpenRouter(
                 #model="anthropic/claude-sonnet-4.6",
-                #model="google/gemini-2.5-pro", 
+                model="google/gemini-2.5-pro", 
                  #model ="openai/gpt-5.4",
-                model="x-ai/grok-4.20",
+                #model="x-ai/grok-4.20",
                 temperature=0.1,
                 max_tokens=30000,
                 openrouter_api_key=os.getenv("OPENROUTER_API_KEY") 
